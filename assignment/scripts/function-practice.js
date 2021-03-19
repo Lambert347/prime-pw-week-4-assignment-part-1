b3914da8001ba13d3039cf955f3b2f7c2e4aac4f
Ian Lambert <lambe347@umn.edu>
2021-03-19 15:28:12 -0500

    Mostly done, still need to add comments and check for small errors

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -13,36 +13,39 @@ console.log('Test - should say "Hello World!"', hello());
 
 
 // 2. Function to return an personalized hello, for example 'Hello, Jo!'
-function helloName( name ) {
-  console.log('Hello, Ian!');
+function helloName(name) {
+  console.log('Hello, ' + name + '!');
   return true;
 }
 // Remember to call the function to test
-helloName();
+helloName('Ian');
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
-  let answer = firstNumber + 9;
+  let answer = firstNumber + secondNumber;
   return answer;
 }
-console.log('First Number + Second Number:', addNumbers(8));
-console.log('First Number + Second Number:', addNumbers(56));
+console.log('First Number + Second Number:', addNumbers(8, 9));
+console.log('First Number + Second Number:', addNumbers(56, 9));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-let answer = 3 * 8 * 4;
+function multiplyThree(firstNumber, secondNumber, thirdNumber) {
+let answer = firstNumber * secondNumber * thirdNumber;
 return answer;
 }
-console.log('Three numbers multiplied should be 96:', multiplyThree());
+console.log('Three numbers multiplied:', multiplyThree(3, 5, 6));
+console.log('Three numbers multiplied:', multiplyThree(5, 86, 102));
+console.log('Three numbers multiplied:', multiplyThree(0, 6, 97));
+console.log('Three numbers multiplied:', multiplyThree(-6, 7, 9));
 
 
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ) {
+function isPositive(number) {
+  if (number > 0) {
     return true;
   } {
     return false;
@@ -57,8 +60,7 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
-function getLast( array )
-{
+function getLast(array) {
   return array[array.length - 1];
 }
 console.log('Last item of array should be 298:', getLast([1, 5, 10, 97, 298]));
@@ -69,12 +71,9 @@ console.log('Last item of array should be 1:', getLast([1]));
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
-function find( value, array )
-{
-  for (let i = 0; i < array.length; i++)
-  {
-    if (array[i] === value)
-    {
+function find(value, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
       return true;
     }
   }
@@ -89,13 +88,10 @@ console.log('Find the value in the array:', find(2, [1, 5, 6, 89, 0, -7, -8]));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string)
-{
-  if (string[0] === letter)
-  {
+function isFirstLetter(letter, string) {
+  if (string[0] === letter) {
     return true;
-  }
-  {
+  } {
     return false;
   }
 }
@@ -103,11 +99,10 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll(array2) {
+function sumAll(array) {
   let sum = 0
-  for (let i = 0; i < array2.length; i++)
-  {
-    sum += array2[i];
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
   return sum;
 }
@@ -119,18 +114,33 @@ console.log('Sum should be 9251', sumAll([6570, 23, 45, 67, 712, 33, 45, 908, 2,
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+
 function givePositive(array) {
-  let newArray = array
-  for (let i = 0; i < newArray.length; i++) {
-    if (newArray[i] < 0 || newArray[i] === 0) {
-      delete newArray[i];
-    }
-  }
-  return newArray;
+  let positive = array;
+  return array.filter(function(number) {
+    return number > 0;
+  });
 }
+
 console.log('New positive Array:', givePositive([1, -7, 0, -2, 1, 6, 7]));
-console.log('New positive Array:', givePositive([-7, -8, 0, -10, -45, -8]));
-console.log('New Positive Array:', givePositive([1, 2, 3, 4, 5, 6, 7]));
+
+
+
+
+
+
+// function givePositive(array) {
+//   let newArray = array
+//   for (let i = 0; i < newArray.length; i++) {
+//     if (newArray[i] < 0 || newArray[i] === 0) {
+//       delete newArray[i];
+//     }
+//   }
+//   return newArray;
+// }
+// console.log('New positive Array:', givePositive([1, -7, 0, -2, 1, 6, 7]));
+// console.log('New positive Array:', givePositive([-7, -8, 0, -10, -45, -8]));
+// console.log('New Positive Array:', givePositive([1, 2, 3, 4, 5, 6, 7]));
 
 //COME BACK TO THIS ONE --> ask in class on thursday
 //Use splice or delete?????????
