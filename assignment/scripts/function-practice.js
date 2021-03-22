e8b9657d085be706461d1dfc95fd1c76157cf56b
Ian Lambert <lambe347@umn.edu>
2021-03-21 23:47:59 -0500

    Ready to submit

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -22,6 +22,7 @@ helloName('Ian');
 
 
 // 3. Function to add two numbers together & return the result
+//Altered the function slighty to take two numbers as inputs, add them together, and return the answer.
 function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
   let answer = firstNumber + secondNumber;
@@ -32,6 +33,7 @@ console.log('First Number + Second Number:', addNumbers(56, 9));
 
 
 // 4. Function to multiply three numbers & return the result
+//Altered the function to take three numbers as inputs, multiply them, and then return the answer.
 function multiplyThree(firstNumber, secondNumber, thirdNumber) {
 let answer = firstNumber * secondNumber * thirdNumber;
 return answer;
@@ -44,6 +46,7 @@ console.log('Three numbers multiplied:', multiplyThree(-6, 7, 9));
 
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
+//Checks if the number is positive and if it is, return true. If not, returns false.
 function isPositive(number) {
   if (number > 0) {
     return true;
@@ -60,6 +63,7 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
+//Function to return the last item of the array, returns undefined if the array in the input is empty.
 function getLast(array) {
   return array[array.length - 1];
 }
@@ -71,6 +75,7 @@ console.log('Last item of array should be 1:', getLast([1]));
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
+//Function that searches an array for a value and returns true if found, false if not found.
 function find(value, array) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === value) {
@@ -88,6 +93,7 @@ console.log('Find the value in the array:', find(2, [1, 5, 6, 89, 0, -7, -8]));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
+//Function to check if the letter of input is first in the array and returns true if that is the case and false if not.
 function isFirstLetter(letter, string) {
   if (string[0] === letter) {
     return true;
@@ -99,6 +105,7 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
+//Function the moves through the entire array with a for loop and adds each value in the array to an increasing sum, then returns the sum.
 function sumAll(array) {
   let sum = 0
   for (let i = 0; i < array.length; i++) {
@@ -114,7 +121,7 @@ console.log('Sum should be 9251', sumAll([6570, 23, 45, 67, 712, 33, 45, 908, 2,
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
+//Function that filters out the positive numbers in an input array and returns those positive values as an array.
 function givePositive(array) {
   let positive = array;
   return array.filter(function(number) {
@@ -123,36 +130,28 @@ function givePositive(array) {
 }
 
 console.log('New positive Array:', givePositive([1, -7, 0, -2, 1, 6, 7]));
+console.log('New positive Array:', givePositive([-7, -8, 0, -10, -45, -8]));
+console.log('New Positive Array:', givePositive([1, 2, 3, 4, 5, 6, 7]));
 
-
-
-
-
-
-// function givePositive(array) {
-//   let newArray = array
-//   for (let i = 0; i < newArray.length; i++) {
-//     if (newArray[i] < 0 || newArray[i] === 0) {
-//       delete newArray[i];
-//     }
-//   }
-//   return newArray;
-// }
-// console.log('New positive Array:', givePositive([1, -7, 0, -2, 1, 6, 7]));
-// console.log('New positive Array:', givePositive([-7, -8, 0, -10, -45, -8]));
-// console.log('New Positive Array:', givePositive([1, 2, 3, 4, 5, 6, 7]));
-
-//COME BACK TO THIS ONE --> ask in class on thursday
-//Use splice or delete?????????
-
-
+//Alternative array to the previous question that deletes non-positive numbers an array and returns the new array of positive numbers.
+function givePositive(array) {
+  let newArray = array
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] < 0 || newArray[i] === 0) {
+      delete newArray[i];
+    }
+  }
+  return newArray;
+}
+console.log('New positive Array:', givePositive([1, -7, 0, -2, 1, 6, 7]));
+console.log('New positive Array:', givePositive([-7, -8, 0, -10, -45, -8]));
+console.log('New Positive Array:', givePositive([1, 2, 3, 4, 5, 6, 7]));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
 
 //The problem I selected is from edabit and it is to make a function that converts minutes into seconds, and then prints the result.
-
 function convertMin(minutes) {
   let seconds = minutes * 60;
   return seconds;
@@ -165,7 +164,6 @@ console.log('Seconds:', convertMin(360));
 
 //That problem was pretty easy so I decided to do another.
 //This next problem is to make a function that returns the area of a triangle with the input of base and height.
-
 function findArea(base, height) {
   let area = (base * height) / 2;
   return area;
